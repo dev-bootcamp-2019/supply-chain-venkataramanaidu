d3a1b1c09d54195664500fef7c7fa9da26c6a59f
venkataramanaidu <ramanaidu1956@gmail.com>
2018-11-30 14:51:00 -0500

    First Commit 11/30/2018

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -1,31 +1,264 @@
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.24;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
-contract TestSupplyChain {
+contract TestSupplyChain  {
+    uint public initialBalance = 1 ether;
 
-    // Test for failing conditions in this contracts
-    // test that every modifier is working
+    SupplyChain public chain;
+    Proxy public sellActor;
+    Proxy public buyActor;
+    Proxy public randomActor;
+
+    string itemName = "Gem";
+    uint256 itemPrice = 3;
+    uint256 itemSku = 0; // the sku will be set to 0
+
+    // allow contract to receive ether
+    function() public payable {}
+
+    function beforeEach() public
+    {
+        // Contract to test
+        chain = new SupplyChain();
+
+        // Sell transaction actor
+        sellActor = new Proxy(chain);
+
+        // Buy transaction actor
+        buyActor = new Proxy(chain);
+
+        // Random transaction actor, neither buyer nor seller
+        randomActor = new Proxy(chain);
+
+        // Seed buyer with some funds
+        // Note: these values are in wei
+        uint256 seedValue = itemPrice + 1;
+        address(buyActor).transfer(seedValue);
+
+        // Seed known item to set contract to `for-sale`
+        sellActor.sell(itemName, itemPrice);
+    }
+
+    function testBuyerAndSellerAreDifferentActors()
+        public
+    {
+        Assert.notEqual(address(buyActor), address(sellActor), "buyer and seller should be different");
+        Assert.equal(address(chain), sellActor.getTarget(), "chain is the target");
+    }
+
+    function testItemCanBePutOnSale()
+        public
+    {
+        // Verify state is `ForSale`
+        address expectedBuyer = address(0);
+        address expectedSeller = address(sellActor);
+
+        string memory name;
+        uint sku;
+        uint price;
+        uint state;
+        address seller;
+        address buyer;
+
+        ( name, sku, price, state, seller, buyer) = chain.fetchItem(itemSku);
+
+        Assert.equal(sku, itemSku, "Item sku is correct");
+        Assert.equal(name, itemName, "Item is named correctly");
+        Assert.equal(price, itemPrice, "Item price correctly");
+        Assert.equal(state, 0, "Item State is `For sale`");
+        Assert.equal(buyer, expectedBuyer, "Item buyer is 0x0");
+        Assert.equal(seller, expectedSeller, "Item seller is the seller`");
+    }
+
+    function getItemState(uint256 _expectedSku)
+        public constant
+        returns (uint256)
+    {
+        string memory name;
+        uint sku;
+        uint price;
+        uint state;
+        address seller;
+        address buyer;
+
+        ( name, sku, price, state, seller, buyer) = chain.fetchItem(_expectedSku);
+        return state;
+    }
 
     // buyItem
+    // test buyer tries to underpay
+    function testUserDoesNotPaysTheRightPrice() public {
+        uint offer = itemPrice - 1; // underfund price
 
-    // test for failure if user does not send enough funds
-    // test for purchasing an item that is not for Sale
+        bool result = buyActor.buy(itemSku, offer);
+        Assert.isFalse(result, "under Paid for item");
 
+        // Verify state is For Sale
+        Assert.equal(getItemState(itemSku), 0, "Item should be `ForSale`");
+    }
+
+    // buyItem
+    // test buyer pays the ask price
+    function testUserPaysTheRightPrice() public {
+        uint offer = itemPrice + 1; // exceed price
+
+        bool result = buyActor.buy(itemSku, offer);
+        Assert.isTrue(result, "Paid the correct price...");
+
+        // Verify state is Sold
+        Assert.equal(getItemState(itemSku), 1, "Item should be `Sold`");
+    }
 
     // shipItem
+    // test some random user cannot Ship item
+    function testRandomUserCannotShipItem() public {
 
-    // test for calls that are made by not the seller
-    // test for trying to ship an item that is not marked Sold
+        // Purchase item
+        uint offer = itemPrice + 1; // exceed price
+        bool result = buyActor.buy(itemSku, offer);
+        Assert.isTrue(result, "Paid the correct price...");
+
+        result = randomActor.ship(itemSku);
+        Assert.isFalse(result, "Non seller cannot ship");
+
+        // Verify state is Sold
+        Assert.equal(getItemState(itemSku), 1, "Item should remain `Sold`");
+    }
+
+    // shipItem
+    // test seller can ship item
+    function testSellerCanShipItem() public {
+        // Purchase item
+        uint offer = itemPrice + 1; // exceed price
+        bool result = buyActor.buy(itemSku, offer);
+        Assert.isTrue(result, "Paid the correct price...");
+
+        result = sellActor.ship(itemSku);
+        Assert.isTrue(result, "seller should be able to ship");
+
+        // Verify state is Shipped
+        Assert.equal(getItemState(itemSku), 2, "Item should be `Shipped`");
+    }
+
+    // shipItem
+    // test that items can't be shipped if they are not sold
+    function testCannotShipAnItemThatIsNotSold() public {
+        // Note: item starts in forSale state
+
+        // Try to ship item
+        bool result = sellActor.ship(itemSku);
+        Assert.isFalse(result, "Cannot ship item that is `ForSale`.");
+
+        // Verify state is ForSale
+        Assert.equal(getItemState(itemSku), 0, "Item should remain `ForSale`");
+    }
 
     // receiveItem
+    // test some random actor cannot receive item
+    function testNonBuyerCannotSetItemAsReceived() public {
+        // Purchase item
+        uint offer = itemPrice + 1; // exceed price
+        bool result = buyActor.buy(itemSku, offer);
+        Assert.isTrue(result, "Paid the correct price...");
 
-    // test calling the function from an address that is not the buyer
-    // test calling the function on an item not marked Shipped
+        // Ship item
+        result = sellActor.ship(itemSku);
+        Assert.isTrue(result, "Seller can ship item that was sold.");
 
-     
+        // Try to receive item
+        result = randomActor.receive(itemSku);
+        Assert.isFalse(result, "Only buyer can receive an item");
+
+        // Verify state is Shipped
+        Assert.equal(getItemState(itemSku), 2, "Item should remain `Shipped`");
+    }
+
+    // receiveItem
+    // test buyer can receive item
+    function testBuyerCanSetItemAsReceived() public {
+        // Purchase item
+        uint offer = itemPrice + 1; // exceed price
+        bool result = buyActor.buy(itemSku, offer);
+        Assert.isTrue(result, "Paid the correct price...");
+
+        // Ship item
+        result = sellActor.ship(itemSku);
+        Assert.isTrue(result, "Seller can ship item that was sold.");
+
+        // Try to receive item
+        result = buyActor.receive(itemSku);
+        Assert.isTrue(result, "Buyer can set receive");
+
+        // Verify state is Shipped
+        Assert.equal(getItemState(itemSku), 3, "Item should be `Received`");
+    }
+
+    // receiveItem
+    // test a buyer cannot recieve an item not shipped
+    function testBuyerCannotReceiveItemNotShipped() public {
+        // Purchase item
+        uint offer = itemPrice + 1; // exceed price
+        bool result = buyActor.buy(itemSku, offer);
+        Assert.isTrue(result, "Paid the correct price...");
+
+        // Try to receive item
+        result = buyActor.receive(itemSku);
+        Assert.isFalse(result, "Buyer can not receive an item thats `sold`");
+
+        // Verify state is Sold
+        Assert.equal(getItemState(itemSku), 1, "Item should be `Sold`");
+    }
+}
 
 
+// Proxy contract Actors for buying and selling
+//
+contract Proxy {
+    address public target;
+
+    constructor(address _target) public { target = _target; }
+
+    // Allow contract to receive ether
+    function() public payable {}
+
+    function getTarget()
+        public constant
+        returns (address)
+    {
+        return target;
+    }
+
+    function sell(string itemName, uint256 itemPrice)
+        public
+    {
+        SupplyChain(target).addItem(itemName, itemPrice);
+    }
+
+    function buy(uint256 sku, uint256 offer)
+        public
+        returns (bool)
+    {
+        // solhint-disable-next-line
+         return address(target).call.value(offer)(abi.encodeWithSignature("buyItem(uint256)", sku));
+
+    }
+
+    function ship(uint256 sku)
+        public
+        returns (bool)
+    {
+        // solhint-disable-next-line
+        return address(target).call(abi.encodeWithSignature("shipItem(uint256)", sku));
+    }
+
+    function receive(uint256 sku)
+        public
+        returns (bool)
+    {
+        // solhint-disable-next-line
+        return address(target).call(abi.encodeWithSignature("receiveItem(uint256)", sku));
+    }
 }
